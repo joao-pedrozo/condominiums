4b9796c65dacb46f9a4e243888621ac5f895cb9b
P <jp.professional@outlook.com>
2024-07-20 20:01:46 -0300

    feat: adiciona formulário base

--- src/app/condominios/adicionar/page.tsx
+++ src/app/condominios/adicionar/page.tsx
@@ -1,6 +1,49 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  nome: z.string(),
+  endereco: z.string(),
+  cnpj: z.string(),
+  quantidadeUnidades: z
+    .number()
+    .int()
+    .positive("A quantidade de unidades deve ser um número positivo"),
+});
 
 export default function AddCondominioPage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      nome: "",
+      endereco: "",
+      cnpj: "",
+      quantidadeUnidades: 0,
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <main>
       <Link
@@ -11,6 +54,67 @@ export default function AddCondominioPage() {
       </Link>
       <h2 className="font-bold text-3xl">Adicionar Condomínio</h2>
       <p className="text-lg mt-1">Adicione um novo condomínio ao sistema.</p>
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="mt-2 grid grid-cols-2 gap-4"
+        >
+          <FormField
+            control={form.control}
+            name="nome"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nome</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="endereco"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Endereço</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="cnpj"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>CNPJ</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="quantidadeUnidades"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Quantidade de unidades</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" type="number" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Adicionar condomínio</Button>
+        </form>
+      </Form>
     </main>
   );
 }
