220d20dc108f3c881d8198fea4616cc4b773d465
P <jp.professional@outlook.com>
2024-07-22 18:32:17 -0300

    feat: adiciona estado de loading na home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Skeleton } from "@/components/ui/skeleton";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import Link from "next/link";
@@ -34,18 +35,20 @@ export default function Home() {
           <div className="grid grid-cols-2 gap-4">
             <div className="bg-muted rounded-lg p-4">
               <h3 className="text-4xl font-bold mb-2">
-                {isLoading ? "..." : condominiums.length}
+                {isLoading ? <span>...</span> : condominiums.length}
               </h3>
               <p className="text-muted-foreground">Condominíos totais</p>
             </div>
             <div className="bg-muted rounded-lg p-4">
               <h3 className="text-4xl font-bold mb-2">
-                {isLoading
-                  ? "..."
-                  : condominiums.reduce(
-                      (acc, curr) => acc + curr.quantidadeUnidades,
-                      0
-                    )}
+                {isLoading ? (
+                  <span>...</span>
+                ) : (
+                  condominiums.reduce(
+                    (acc, curr) => acc + curr.quantidadeUnidades,
+                    0
+                  )
+                )}
               </h3>
               <p className="text-muted-foreground">Unidades totais</p>
             </div>
@@ -55,7 +58,17 @@ export default function Home() {
           <h3 className="text-xl font-bold mb-4">Recentemente Adicionados</h3>
           <ul className="space-y-4">
             {isLoading ? (
-              <span>...</span>
+              <>
+                <li>
+                  <Skeleton className="w-full h-[60px] rounded-md" />
+                </li>
+                <li>
+                  <Skeleton className="w-full h-[60px] rounded-md" />
+                </li>
+                <li>
+                  <Skeleton className="w-full h-[60px] rounded-md" />
+                </li>
+              </>
             ) : (
               condominiums.slice(0, 3).map((condominium) => (
                 <li
