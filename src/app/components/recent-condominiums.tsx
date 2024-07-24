e8d1d66d67b6b561a190a1f402906f3bb8661393
P <jp.professional@outlook.com>
2024-07-24 14:47:45 -0300

    refactor: early return em condomínios recentes

--- src/app/components/recent-condominiums.tsx
+++ src/app/components/recent-condominiums.tsx
@@ -11,28 +11,28 @@ export default function RecentCondominiums({
   condominiums,
   isLoading,
 }: RecentCondominiumsProps) {
+  if (isLoading) {
+    return (
+      <div className="bg-card rounded-lg shadow-sm">
+        <h3 className="text-xl font-bold mb-4">Recentemente Adicionados</h3>
+        <ul className="space-y-4">
+          {[...Array(3)].map((_, index) => (
+            <li key={index}>
+              <Skeleton className="w-full h-[60px] rounded-md" />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
   return (
     <div className="bg-card rounded-lg shadow-sm">
       <h3 className="text-xl font-bold mb-4">Recentemente Adicionados</h3>
       <ul className="space-y-4">
-        {isLoading ? (
-          <>
-            {[...Array(3)].map((_, index) => (
-              <li key={index}>
-                <Skeleton className="w-full h-[60px] rounded-md" />
-              </li>
-            ))}
-          </>
-        ) : (
-          condominiums
-            .slice(0, 3)
-            .map((condominium) => (
-              <RecentCondominium
-                key={condominium.id}
-                condominium={condominium}
-              />
-            ))
-        )}
+        {condominiums.slice(0, 3).map((condominium) => (
+          <RecentCondominium key={condominium.id} condominium={condominium} />
+        ))}
       </ul>
     </div>
   );
