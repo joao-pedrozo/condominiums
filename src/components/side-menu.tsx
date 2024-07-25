2df087a1ac3b5d4e8d881e993acdd88e392b86ac
Pedro <jp.pedrozo@hotmail.com>
2024-07-25 12:58:40 -0300

    feat: adiciona icons no side-menu

--- src/components/side-menu.tsx
+++ src/components/side-menu.tsx
@@ -2,9 +2,10 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { cva } from 'class-variance-authority';
 import React from 'react';
+import { BarChart, NotebookTabs } from 'lucide-react';
 
 const linkStyles = cva(
-  'text-lg font-semibold rounded-md mb-[4px] py-2 px-4 hover:bg-blue-600 hover:text-blue-600 hover:bg-opacity-10',
+  'flex gap-2 items-center text-lg font-semibold rounded-md mb-[4px] py-2 px-4 hover:bg-blue-600 hover:text-blue-600 hover:bg-opacity-10',
   {
     variants: {
       active: {
@@ -19,8 +20,8 @@ const linkStyles = cva(
 );
 
 const menuItems = [
-  { href: '/', label: 'Dashboard' },
-  { href: '/condominios', label: 'Gestão de Condomínios' },
+  { href: '/', label: 'Dashboard', Icon: BarChart },
+  { href: '/condominios', label: 'Gestão de Condomínios', Icon: NotebookTabs },
 ];
 
 const SideMenu = () => {
@@ -34,8 +35,9 @@ const SideMenu = () => {
             <Link href="/">Lobie</Link>
           </h1>
         </li>
-        {menuItems.map(({ href, label }) => (
+        {menuItems.map(({ href, label, Icon }) => (
           <li key={href} className={linkStyles({ active: pathname === href })}>
+            <Icon size={22} />
             <Link href={href}>{label}</Link>
           </li>
         ))}
